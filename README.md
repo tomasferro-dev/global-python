7e0399e08b14f723877c08de1de1e8fe28ab18cb
tomas ferro <tomasferro.dev@gmail.com>
2024-11-19 20:34:27 -0300

    Ordenar codigo

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # global-python
+
+    matriz_adn = ["AGATCA", "GATTCA", "CAACAT", "GAGCTA", "ATTGCG", "CTGTTC"]
+    mutante_horizontal = ["TTTTCA", "GATTCA", "CAACAT", "GAGCTA", "ATTGCG", "CTGTTC"]
+    mutante_vertical = ["AGATCA", "GATTCA", "CAATAT", "GAGTTA", "ATTGCG", "CTGTTC"]
+    mutante_diagonal = ["TGATCA", "GTTTCA", "CATCAT", "GAGTTA", "ATTGCG", "CTGTTC"]
--- ejecutable.py
+++ ejecutable.py
@@ -4,11 +4,6 @@ from virus import Virus
 from sanador import Sanador
 
 def main():
-    # Ejemplo de uso de las clases
-    detector = Detector("atributo1", "atributo2")
-    radiacion = Radiacion("A", "atributo1", "atributo2")
-    # virus = Virus("T", "atributo1", "atributo2")
-    # sanador = Sanador("atributo1", "atributo2")
     
     matriz_adn = ["AGATCA", "GATTCA", "CAACAT", "GAGCTA", "ATTGCG", "CTGTTC"]
     mutante_horizontal = ["TTTTCA", "GATTCA", "CAACAT", "GAGCTA", "ATTGCG", "CTGTTC"]
@@ -17,12 +12,15 @@ def main():
 
     #********************************************************************
     # Ejemplo para detectar mutacion
-    # es_mutante = detector.detectar_mutantes(mutante_vertical)
-    # print(f"¿Es mutante?: {es_mutante}")
+    detector = Detector("atributo1", "atributo2")
+    es_mutante = detector.detectar_mutantes(matriz_adn)
+    mensaje_es_mutante = lambda: "Si, es mutante" if (es_mutante) else "No, no es mutante";
+    print(f"¿Es mutante?: {mensaje_es_mutante()}")
     #********************************************************************
     
     #********************************************************************
     # Ejemplo de radiacion
+    # radiacion = Radiacion("A", "atributo1", "atributo2")
     # matriz_mutante_horizontal = radiacion.crear_mutante("T", (0, 0), "V")
     # for fila in matriz_mutante_horizontal:
     #     print("".join(fila))
@@ -38,11 +36,11 @@ def main():
 
     #********************************************************************
     #Ejemplo de sanador
-    sanador = Sanador("atributo1", "atributo2")
-    matriz_adn_sanada = sanador.sanar_mutantes(mutante_horizontal)
-    print("Matriz de ADN sanada:")
-    for fila in matriz_adn_sanada:
-        print(fila)
+    # sanador = Sanador("atributo1", "atributo2")
+    # matriz_adn_sanada = sanador.sanar_mutantes(mutante_horizontal)
+    # print("Matriz de ADN sanada:")
+    # for fila in matriz_adn_sanada:
+    #     print(fila)
 
 
 
